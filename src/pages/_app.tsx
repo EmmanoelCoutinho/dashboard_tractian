038e2d2b9693a231ce79201389db23c5226e8366
EmmanoelCoutinho <emmanoelcesar967@gmail.com>
2021-04-14 18:55:13 -0300

    config project add styled components provider with themes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,20 @@ import App from 'next/app';
 
 import GlobalStyles from '../styles/globalStyles';
 
+//themes and provider for switch the theme
+import { light } from '../styles/themes/light';
+import { ThemeProvider } from 'styled-components';
+
 export default class MyApp extends App {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
   render() {
     const { Component, pageProps } = this.props;
     return (
       <>
-        <GlobalStyles />
-        <Component {...pageProps} />
+        <ThemeProvider theme={light}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
       </>
     );
   }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
 import React from 'react';
 
+import { Container } from '../styles/stylesIndex/styles';
+import Header from '../components/Header/index';
+
 const Home: React.FC = () => {
-  return <h1>Home</h1>;
+  return (
+    <Container>
+      <Header />
+    </Container>
+  );
 };
 
 export default Home;
